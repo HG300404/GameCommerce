76ce955c0451c96f346c908b58dbeb0cb9a08ce9
Phương Linh <linhnnp.22ite@vku.udn.vn>
2025-12-06 20:28:36 +0700

    scrum 93: FE Admin Order

--- backend/src/routes/OrderRouter.js
+++ backend/src/routes/OrderRouter.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/OrderController");
-const {
-  authUserMiddleware,
-  authMiddleware,
-} = require("../middleware/authMiddleware");
+
 
 router.post("/create", orderController.createOrder);
 router.get("/get-order-details/:id", orderController.getDetailsOrder);
