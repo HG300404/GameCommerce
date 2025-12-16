6d33a2c7411ff79104bb414881f069380bd40eee
Phương Linh <linhnnp.22ite@vku.udn.vn>
2025-12-16 15:18:34 +0700

    scrum 56: user infor

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -7,6 +7,10 @@ import OrderPage from "../pages/OrderPage/OrderPage.jsx";
 import PaymentPage from "../pages/PaymentPage/PaymentPage.jsx";
 import OrderSuccess from "../pages/OrderSuccess/OrderSuccess.jsx";
 import MyOrder from "../pages/MyOrder/MyOrder.jsx";
+import ProfilePage from "../pages/Profile/ProfilePage.jsx";
+import TypeProductPage from "../pages/TypeProductPage/TypeProductPage.jsx";
+import ProductsPage from "../pages/ProductsPage/ProductsPage.jsx";
+
 export const routes = [
   {
     path: "/",
@@ -53,5 +57,20 @@ export const routes = [
     path: "/myOrder",
     page: MyOrder,
     isShowHeader: true,
-  }
+  },
+  {
+    path: "/profile-user",
+    page: ProfilePage,
+    isShowHeader: true,
+  },
+  {
+    path: "/products",
+    page: ProductsPage,
+    isShowHeader: true,
+  },
+  {
+    path: "/type",
+    page: TypeProductPage,
+    isShowHeader: true,
+  },
 ];
